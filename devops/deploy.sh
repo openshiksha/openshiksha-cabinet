dc7849cbe327b77e053dbcf6551cf170b6e33a44
Oasis Vali <oasis.vali@gmail.com>
2015-09-11 14:23:55 +0530

    deploy script updated for standalone server

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Usage: scripts/deploy.sh (from hwcentral-cabinet root dir)
+# Usage: devops/deploy.sh (from hwcentral-cabinet root dir)
 
+nginx -s stop
 git pull origin master
 sudo find ./ -type d -exec chmod 777 {} +
 sudo find ./ -type f -exec chmod 666 {} +
+nginx
