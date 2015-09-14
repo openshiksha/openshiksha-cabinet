47acab0e0ed55ca8d72e52bbf8d6a4359e676dc8
Oasis Vali <oasis.vali@gmail.com>
2015-09-14 16:07:30 +0530

    devops restructuring

--- devops/backup.sh
+++ devops/backup.sh
@@ -3,7 +3,7 @@
 # Usage: devops/backup.sh (from hwcentral-cabinet root dir)
 
 # create the database dump which will be backed up by rackspace daily
-mysqldump --single-transaction -uhwcreadonly -p$(cat /etc/hwcentral/db_read_password.txt) hwcentral-qa > ../db_bak/hwcentral-qa.sql
+mysqldump --single-transaction -uhwcreadonly -p$(cat /etc/hwcentral/db_read_password.txt) hwcentral_prod > ../db_bak/hwcentral_prod.sql
 
 # push all changes in the cabinet git repository to trunk
 git add .
