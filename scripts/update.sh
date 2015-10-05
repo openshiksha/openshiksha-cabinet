ec5e2ce490b5a153fe97db0f36bc12ac75c8d5ea
Oasis Vali <oasis.vali@gmail.com>
2015-10-05 14:12:41 +0530

    update script ready

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,8 @@
 echo 'Stopping nginx...'
 sudo nginx -s stop
 
-echo 'This will delete your local changes (INCLUDING SUBMISSIONS) that your cabinet contains. Press Ctrl+C to quit'
+echo 'This will delete your local changes (INCLUDING SUBMISSIONS) that your cabinet contains!'
+echo 'Press Ctrl+C to quit. Press Enter to continue'
 read -p "$*"
 
 git reset --hard HEAD
@@ -14,6 +15,6 @@ git clean -df
 git pull origin master
 
 echo 'updating the nginx conf to use current username'
-sed -i s/oasis/sidhant/g devops/nginx.conf
+sed -i s/oasis/$USER/g devops/nginx.conf
 echo 'Restarting nginx...'
 sudo nginx
