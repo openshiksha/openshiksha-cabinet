75bba9a25966df72c48d2a870a7cdb2b1ad8919a
Oasis Vali <oasis.vali@gmail.com>
2015-10-07 19:49:54 +0530

    added message to backup script

--- devops/backup.sh
+++ devops/backup.sh
@@ -2,6 +2,7 @@
 
 # Usage: devops/backup.sh (from hwcentral-cabinet root dir)
 
+echo 'Dumping db state to backup file...'
 # create the database dump which will be backed up by rackspace daily
 mysqldump --single-transaction -uhwcreadonly -p$(cat /etc/hwcentral/db_read_password.txt) hwcentral_prod > ../db_bak/hwcentral_prod.sql
 
