a51f051e27d541cff6d4e96e933f27d7e300b850
Oasis Vali <oasis.vali@gmail.com>
2015-10-21 20:03:02 +0530

    qa devops parity

--- devops/backup.sh
+++ devops/backup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Usage: devops/backup.sh (from hwcentral-cabinet root dir)
+# Usage: hwcentral-cabinet/devops/backup.sh (from home dir)
+
+cd hwcentral-cabinet
 
 echo "Running backup script on $(date)" >> backup-tmp.log 2>&1
 
 echo 'Dumping db state to backup file...' >> backup-tmp.log 2>&1
 
 # create the database dump which will be backed up by rackspace daily
-mysqldump --single-transaction -uhwcreadonly -p$(cat /etc/hwcentral/db_read_password.txt) hwcentral_prod 2>> backup-tmp.log 1> ../db_bak/hwcentral_prod.sql
+mysqldump --single-transaction -u$(cat /etc/hwcentral/db_backup_user.txt) -p$(cat /etc/hwcentral/db_backup_password.txt) $(cat /etc/hwcentral/db_name.txt) 2>> backup-tmp.log 1> ../db_bak/hwcentral_prod.sql
 
 echo 'db state dumped ->' >> backup-tmp.log 2>&1
 ls -alt ../db_bak/ >> backup-tmp.log 2>&1
@@ -19,7 +21,7 @@ git push origin master >> backup-tmp.log 2>&1
 
 # send log email
 echo 'Sending log email'
-curl -s --user "api:$(cat /etc/hwcentral/mailgun_apikey.txt)" https://api.mailgun.net/v3/hwcentral.in/messages -F from='DB Server <postmaster@hwcentral.in>' -F to=exception@hwcentral.in -F subject='Nightly Backup' -F text="$(cat backup-tmp.log)"
+curl -s --user "api:$(cat /etc/hwcentral/mailgun_apikey.txt)" https://api.mailgun.net/v3/$(cat /etc/hwcentral/mailgun_domain.txt)/messages -F from="$(cat /etc/hwcentral/mailgun_from.txt)" -F to=exception@hwcentral.in -F subject='Nightly Backup' -F text="$(cat backup-tmp.log)"
 
 # print log so that cron will append it to log file
 cat backup-tmp.log
--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -6,8 +6,8 @@ echo 'updating the nginx conf to use current username'
 sed -i s/oasis/$USER/g devops/nginx.conf
 
 echo 'updating the cabinet endpoints for docker setup'
-sed -i "s/allow 10.176.5.116/allow $DOCKER_HOST_IP/" devops/nginx.conf
-sed -i "s/server_name 10.176.7.252/server_name localhost/" devops/nginx.conf
+sed -i "s/allow 10.130.67.205/allow $DOCKER_HOST_IP/" devops/nginx.conf
+sed -i "s/server_name 10.130.97.154/server_name localhost/" devops/nginx.conf
 
 echo 'enabling delete dav method for dev testing'
 sed -i "s/dav_methods PUT/dav_methods PUT DELETE/" devops/nginx.conf
