c03c780aa2b9819f643a97db8ebafa0162ba7ee3
Oasis Vali <oasis.vali@gmail.com>
2016-02-04 16:38:33 +0530

    update script customization for prod

--- scripts/update.sh
+++ scripts/update.sh
@@ -18,11 +18,17 @@ echo 'updating the nginx conf to use current username'
 sed -i s/oasis/$USER/g devops/nginx.conf
 
 echo 'updating the cabinet endpoints for localhost setup'
-sed -i "s/allow 10.130.67.205/allow 127.0.0.1/" devops/nginx.conf
-sed -i "s/server_name 10.130.97.154/server_name localhost/" devops/nginx.conf
+sed -i "s/allow 10.176.5.116/allow 127.0.0.1/" devops/nginx.conf
+sed -i "s/server_name 10.176.7.252/server_name localhost/" devops/nginx.conf
 
 echo 'enabling delete dav method for dev testing'
 sed -i "s/dav_methods PUT/dav_methods PUT DELETE/" devops/nginx.conf
+
+echo 'switching the current repo to be the local cabinet (if it isnt already)'
+if [ "$HOME/hwcentral-cabinet" != "${PWD}"]; then
+    mv $HOME/hwcentral-cabinet $HOME/hwcentral-cabinet-qa
+    mv ${PWD} $HOME/hwcentral-cabinet
+fi
 
 echo 'Restarting nginx...'
 sudo nginx
